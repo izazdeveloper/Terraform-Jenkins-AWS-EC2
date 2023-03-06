e5c58952dc46ede74d24f735e15e87e3c44f182b
izazdeveloper <55899273+izazdeveloper@users.noreply.github.com>
2023-03-06 01:45:13 +0100

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,13 +1,18 @@
-variable "access_key" {}
-variable "secret_key" {}
 variable "region" {
+  type    = "string"
   default = "us-east-1"
 }
 variable "ami_id" {
   type = "map"
   default = {
-    us-east-1    = "ami-035b3c7efe6d061d5"
-    eu-west-2    = "ami-132b3c7efe6sdfdsfd"
-    eu-central-1 = "ami-9787h5h6nsn"
+    us-east-1    = "ami-00dc79254d0461090"
   }
+}
+variable "instance_type" {
+  type    = "string"
+  default = "t2.micro"
+}
+variable "key_name" {
+  type    = "string"
+  default = "ec2-demo"
 }
